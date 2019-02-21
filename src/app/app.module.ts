fe5eebf629ea7030e6ba9792703a20ba68673292
melnykvo <valeriy.melnyk@fuib.com>
2019-02-21 11:26:20 +0200

    -add: @honey/styleshhets dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FuuiModule } from 'projects/fuui/src/public_api';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FuuiModule } from 'projects/honey-ng/src/public_api';
 
 @NgModule({
   declarations: [
