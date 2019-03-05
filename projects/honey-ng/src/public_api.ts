fc21c0f122a6baf5dd2410b29b4ed9c78c909f0a
melnykvo <valeriy.melnyk@fuib.com>
2019-03-05 12:34:13 +0200

    Fix export dialog service and dialog shred interfaces

--- projects/honey-ng/src/public_api.ts
+++ projects/honey-ng/src/public_api.ts
@@ -3,4 +3,8 @@
  */
 
 export * from './lib/hn.module';
+
+// Dialog
 export * from './lib/modules/dialog/dialog.module';
+export * from './lib/modules/dialog/shared/dialog';
+export * from './lib/modules/dialog/services/dialog-service';
