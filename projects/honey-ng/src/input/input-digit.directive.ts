36cb477d665c19bdfb9c940588ce8f0b196f4322
trygubv <Volodymyr.Trygub@fuib.com>
2019-03-21 09:31:42 +0200

    refactoring

--- projects/honey-ng/src/input/input-digit.directive.ts
+++ projects/honey-ng/src/input/input-digit.directive.ts
@@ -12,36 +12,31 @@ import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
     }
   ]
 })
-export class InputDigitDirective implements ControlValueAccessor, OnInit {
-
-
-  hnInput: HnInput;
-
-  get value(): string {
-    return this.hnInput.value.value;
-  }
-
-  constructor(
-    private renderer: Renderer2,
-    private inputElementRef: ElementRef
-  ) { }
-
-  ngOnInit() {
-    this.hnInput = new HnInput(this.inputElementRef.nativeElement, this.renderer, this.cleanFunction);
-  }
-
-  writeValue(value: string) {
-    this.hnInput.writeValue(value);
-  }
-
-  registerOnChange(fn: Function) {
-    this.hnInput.registerOnChange(fn);
-  }
-
-  registerOnTouched(fn: Function) {
-    this.hnInput.registerOnTouched(fn);
-  }
+export class InputDigitDirective extends HnInput implements ControlValueAccessor {
 
   private cleanFunction: CleanFunction = (inputValue: string): string => inputValue.replace(/[^0-9]/g, '');
+  hnInput: HnInput;
 
+  // get value(): string {
+  //   return this.hnInput.value.value;
+  // }
+
+  constructor(
+    renderer: Renderer2,
+    inputElementRef: ElementRef
+  ) {
+    super(inputElementRef, renderer, this.cleanFunction)
+  }
+
+  // writeValue(value: string) {
+  //   this.hnInput.writeValue(value);
+  // }
+
+  // registerOnChange(fn: Function) {
+  //   this.hnInput.registerOnChange(fn);
+  // }
+
+  // registerOnTouched(fn: Function) {
+  //   this.hnInput.registerOnTouched(fn);
+  // }
 }
