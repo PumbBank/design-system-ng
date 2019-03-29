fd2e88bfbaa0d7142516590ef50faa85984a7742
popenkov <popenkov@fuib.com>
2019-03-29 09:33:46 +0200

    -add: ControlValueAccessor to select

--- projects/honey-ng/src/select/components/select/select.component.ts
+++ projects/honey-ng/src/select/components/select/select.component.ts
@@ -1,35 +1,74 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'hn-select',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectComponent),
+      multi: true,
+    }
+  ],
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.scss']
 })
-export class SelectComponent<T = any> implements OnInit {
+export class SelectComponent<T = any> implements ControlValueAccessor {
+  private options: Map<any, string> = new Map<any, string>();
 
   @Input() selected: T;
   @Output() selectedChange: EventEmitter<T> = new EventEmitter<T>();
 
-  selectedCaption: string;
+  get selectedCaption(): string {
+    return this.options.get(this.selected);
+  }
   active: boolean = false;
 
-  constructor(
-  ) { }
+  constructor() { }
 
-  ngOnInit() {
-  }
-
-  setSelected(option: T, caption: string): void {
+  setSelected(option: T): void {
     this.selected = option;
-    this.selectedCaption = caption;
     this.selectedChange.emit(option);
+    this.onChange(this.selected);
   }
 
   open(): void {
+    if (!this.options.has(this.selected)) {
+      this.selected = null;
+    }
     this.active = true;
+    this.onTouched();
   }
 
   close(): void {
     this.active = false;
+  }
+
+  writeValue(value: any): void {
+    if (!this.options.has(value)) {
+      this.selected = null;
+      this.onChange(null);
+    } else {
+     this.selected = value;
+    }
+  }
+
+  onChange: any = () => {};
+  onTouched: any = () => { };
+
+  registerOnChange(fn) {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn) {
+    this.onTouched = fn;
+  }
+
+  registrateOption(value: any, caption: string) {
+    this.options.set(value, caption);
+  }
+
+  destroyOption(value: any) {
+    this.options.delete(value);
   }
 }
