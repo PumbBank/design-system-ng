fa77884ecff195b10350afa6ec8da31ae26bd4ee
trygubv <Volodymyr.Trygub@fuib.com>
2019-04-11 16:34:46 +0300

    add export modules

--- projects/honey-ng/src/public_api.ts
+++ projects/honey-ng/src/public_api.ts
@@ -1,3 +1,5 @@
 export * from './hn/hn.module';
 export * from './dialog/public_api';
 export * from './table/public_api';
+export * from './button/button.module';
+export * from './approve/approve.module';
