2787be0a00eccd7857d356511ec62492c316a25d
trygubv <Volodymyr.Trygub@fuib.com>
2019-08-08 14:35:14 +0300

    update input markup

--- projects/honey-ng/src/input/hn-input.ts
+++ projects/honey-ng/src/input/hn-input.ts
@@ -1,4 +1,4 @@
-import { Renderer2, OnChanges, SimpleChanges, Input, OnDestroy } from '@angular/core';
+import { Renderer2, OnChanges, SimpleChanges, Input, OnDestroy, ɵConsole, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { ErrorMessageHelper } from '../utils/error-message.helper';
 import { ValidationErrors } from '@angular/forms';
@@ -8,14 +8,20 @@ export type CleanFunction = (inputValue: any) => string;
 const DEFAULT_CLEN_FUNCTION = (inputValue: any): string => inputValue;
 
 export class HnInput implements OnChanges, OnDestroy {
+
   protected cleanFunction: CleanFunction = DEFAULT_CLEN_FUNCTION;
 
-  @Input()
-  errors: ValidationErrors | null = null;
+  @Input() errors: ValidationErrors | null = null;
+  @Input() caption: string = '';
 
   wrapperElement: HTMLElement;
   captionElement: HTMLElement;
-  errorsElement: HTMLElement;
+  bodyElement: HTMLElement;
+  entranceElement: HTMLElement;
+  footerElement: HTMLElement;
+  msgWrapperElement: HTMLElement;
+  msgIconElement: HTMLElement;
+  msgTextElement: HTMLElement;
 
   value: BehaviorSubject<string> = new BehaviorSubject<string>(null);
   mutationObserver: MutationObserver;
@@ -25,7 +31,7 @@ export class HnInput implements OnChanges, OnDestroy {
     private renderer: Renderer2,
   ) {
     this.createDom();
-    this.updateCaptionState();
+    // this.updateCaptionState();
     this.watchInputValueChanges();
     this.watchTouches();
     this.watchValidationChangesByClassName();
@@ -35,6 +41,9 @@ export class HnInput implements OnChanges, OnDestroy {
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.errors) {
       this.errorsUpdateText();
+    }
+    if (changes.caption) {
+      this.captionUpdateText();
     }
   }
 
@@ -59,7 +68,7 @@ export class HnInput implements OnChanges, OnDestroy {
     }
 
     this.input.value = cleanValue;
-    this.updateCaptionState();
+    // this.updateCaptionState();
   }
 
   onChangeCallback: Function = () => { };
@@ -67,19 +76,21 @@ export class HnInput implements OnChanges, OnDestroy {
 
   private updateValidationState(invalid: boolean = false): void {
     if (invalid) {
-      this.renderer.addClass(this.wrapperElement, 'hn-input_warn');
+      this.renderer.addClass(this.wrapperElement, 'input_error');
+      this.renderer.appendChild(this.footerElement, this.msgWrapperElement);
     } else {
-      this.renderer.removeClass(this.wrapperElement, 'hn-input_warn');
+      this.renderer.removeClass(this.wrapperElement, 'input_error');
+      this.renderer.removeChild(this.footerElement, this.msgWrapperElement);
     }
   }
 
-  private updateCaptionState(): void {
-    if (this.input.value) {
-      this.renderer.addClass(this.wrapperElement, 'hn-input_filled');
-    } else {
-      this.renderer.removeClass(this.wrapperElement, 'hn-input_filled');
-    }
-  }
+  // private updateCaptionState(): void {
+  //   if (this.input.value) {
+  //     this.renderer.addClass(this.wrapperElement, 'hn-input_filled');
+  //   } else {
+  //     this.renderer.removeClass(this.wrapperElement, 'hn-input_filled');
+  //   }
+  // }
 
   private watchInputValueChanges(): void {
     this.input.addEventListener('input', () => {
@@ -90,7 +101,7 @@ export class HnInput implements OnChanges, OnDestroy {
       }
 
       this.onChangeCallback(cleanValue);
-      this.updateCaptionState();
+      // this.updateCaptionState();
     });
   }
 
@@ -109,30 +120,45 @@ export class HnInput implements OnChanges, OnDestroy {
   }
 
   private createDom(): void {
-    this.captionElement = this.renderer.createElement('div');
     this.wrapperElement = this.renderer.createElement('label');
-    this.errorsElement = this.renderer.createElement('div');
-
-    this.captionElement.innerHTML = this.input.getAttribute('placeholder') || '';
+    this.captionElement = this.renderer.createElement('div');
+    this.bodyElement = this.renderer.createElement('div');
+    this.entranceElement = this.renderer.createElement('div');
+    this.footerElement = this.renderer.createElement('div');
+    this.msgWrapperElement = this.renderer.createElement('div');
+    this.msgIconElement = this.renderer.createElement('div');
+    this.msgTextElement = this.renderer.createElement('div');
 
     this.renderer.insertBefore(this.input.parentElement, this.wrapperElement, this.input);
     this.renderer.appendChild(this.wrapperElement, this.captionElement);
-    this.renderer.appendChild(this.wrapperElement, this.input);
-    this.renderer.appendChild(this.wrapperElement, this.errorsElement);
+    this.renderer.appendChild(this.wrapperElement, this.bodyElement);
+    this.renderer.appendChild(this.wrapperElement, this.footerElement);
 
-    this.renderer.addClass(this.wrapperElement, 'hn-input');
-    this.renderer.addClass(this.input, 'hn-input__input');
-    this.renderer.addClass(this.captionElement, 'hn-input__caption');
-    this.renderer.addClass(this.errorsElement, 'hn-input__hint');
-    this.renderer.addClass(this.errorsElement, 'hn-input__hint_warn');
+    this.renderer.appendChild(this.bodyElement, this.entranceElement);
+    this.renderer.appendChild(this.entranceElement, this.input);
 
-    this.mutationObserver = new MutationObserver(() => {
-      this.captionElement.innerHTML = this.input.getAttribute('placeholder') || '';
-    });
-    this.mutationObserver.observe(this.input, {attributes: true});
+    this.renderer.appendChild(this.msgWrapperElement, this.msgIconElement);
+    this.renderer.appendChild(this.msgWrapperElement, this.msgTextElement);
+
+    this.renderer.addClass(this.wrapperElement, 'input');
+    this.renderer.addClass(this.captionElement, 'input__label');
+    this.renderer.addClass(this.bodyElement, 'input__body');
+    this.renderer.addClass(this.entranceElement, 'input__enterence');
+    this.renderer.addClass(this.input, 'input__input');
+    this.renderer.addClass(this.footerElement, 'input__footer');
+    this.renderer.addClass(this.msgWrapperElement, 'control-message');
+    this.renderer.addClass(this.msgWrapperElement, 'input__control-message');
+    this.renderer.addClass(this.msgIconElement, 'control-message__icon');
+    this.renderer.addClass(this.msgIconElement, 'icon');
+    this.renderer.addClass(this.msgIconElement, 'icon_info');
+    this.renderer.addClass(this.msgTextElement, 'control-message__text');
   }
 
   private errorsUpdateText() {
-    this.errorsElement.innerText = this.errors ? ErrorMessageHelper.getMessage(this.errors) : '';
+    this.msgTextElement.innerText = this.errors ? ErrorMessageHelper.getMessage(this.errors) : '';
+  }
+
+  private captionUpdateText() {
+    this.captionElement.innerText = this.caption || '';
   }
 }
