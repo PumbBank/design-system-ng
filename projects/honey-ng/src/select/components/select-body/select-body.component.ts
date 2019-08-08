c8771c946ef57dfc83e92e639bc209843a4e9c20
popenkov <popenkov@fuib.com>
2019-08-08 16:30:23 +0300

    fix select style

--- projects/honey-ng/src/select/components/select-body/select-body.component.ts
+++ projects/honey-ng/src/select/components/select-body/select-body.component.ts
@@ -9,6 +9,7 @@ import { SelectComponent } from '../select/select.component';
 export class SelectBodyComponent implements OnInit {
 
   get active(): boolean {
+    console.log(this.selectComponent.active);
     return this.selectComponent.active;
   }
 
