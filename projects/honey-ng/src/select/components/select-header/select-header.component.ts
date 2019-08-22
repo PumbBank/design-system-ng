c02d6326c240a723f0e8d87d0e7d28d56d2fcca6
popenkov <popenkov@fuib.com>
2019-08-22 12:11:21 +0300

    add filter to select-header.component

--- projects/honey-ng/src/select/components/select-header/select-header.component.ts
+++ projects/honey-ng/src/select/components/select-header/select-header.component.ts
@@ -10,6 +10,7 @@ import { SelectComponent } from '../select/select.component';
 export class SelectHeaderComponent<T = any> implements OnInit {
   @Input() caption: string = 'Select';
   @Input() placeholder: string = 'Select';
+  @Input() filter: boolean = false;
 
   get active(): boolean {
     return this.selectComponent.active;
