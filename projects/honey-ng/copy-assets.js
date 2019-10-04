74692ffae4b64cd2ce5b612760409f1229037fdf
melnykvo <valeriy.melnyk@fuib.com>
2019-10-04 13:06:36 +0300

    Commit

--- projects/honey-ng/copy-assets.js
+++ projects/honey-ng/copy-assets.js
@@ -1,10 +1,9 @@
 const fs = require('fs-extra');
 const dest = require('./ng-package.json').dest;
 
-console.log(__dirname + '\\src\\assets');
-fs.copy(__dirname + '\\src\\assets', __dirname + '..\\..\\..\\dist\\honey-ng\\assets', function (err) {
+fs.copy(__dirname + '/src/assets', __dirname + '../../../dist/honey-ng/assets', function (err) {
   if (err) {
-    console.error(err);
+    throw new Error(JSON.stringify(err));
   } else {
     console.log("success!");
   }
