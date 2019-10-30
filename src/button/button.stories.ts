517a08c57ef072c2f8a8b995f43973c99b4a8237
kostenvo <volodymyr.kostenko@fuib.com>
2019-10-30 21:41:19 +0200

    DSYS-80: add addon Knobs

--- src/button/button.stories.ts
+++ src/button/button.stories.ts
@@ -1,17 +1,25 @@
+import { storiesOf } from '@storybook/angular';
+import { withKnobs, text } from "@storybook/addon-knobs";
 import { ButtonComponent } from './components/button/button.component';
 
-export default { title: 'My Button' };
+storiesOf('Addon|Knobs', module)
+  .addParameters({
+    knobs: {
+      disableDebounce: true,
+    },
+  })
+  .addDecorator(withKnobs)
+  .add('Simple', () => {
+    const label = text('label', 'Regular Button');
 
-export const withText = () => ({
-  moduleMetadata: {
-    declarations: [
-      ButtonComponent
-    ]
-  },
-  props: {
-    text: 'Hello Button',
-  },
-  template: `
-    <mill-button>{{text}}</mill-button>
-  `
-});
+    return {
+      moduleMetadata: {
+        entryComponents: [ButtonComponent],
+        declarations: [ButtonComponent],
+      },
+      template: `<mill-button>{{label}}</mill-button>`,
+      props: {
+        label,
+      },
+    };
+  });
