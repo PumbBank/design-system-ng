2a5b3d1ca697a77dd96e69797e3dc61d1a5a0420
avdeenkd <dmyto.avdeenko@fuib.com>
2020-02-18 15:36:50 +0200

    DSYS-105: Badges and Icons

--- src/badge/badge.module.ts
+++ src/badge/badge.module.ts
@@ -1,14 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BadgeComponent } from './components/badge/badge.component';
+import { BadgeIconService } from './services/badge-icon.service';
+import { BadgeIconDirective } from './directives/badge-icon.directive';
 
 
 @NgModule({
-	declarations: [BadgeComponent],
-	exports: [BadgeComponent],
+	declarations: [BadgeComponent, BadgeIconDirective],
+	exports: [BadgeComponent, BadgeIconDirective],
 	imports: [
 		CommonModule,
 	],
+  providers: [BadgeIconService]
 })
-export class BadgeModule { }
+export class BadgeModule {
+  constructor(
+    @Optional() @SkipSelf() parentModule: BadgeModule,
+    private _badgeIconService: BadgeIconService
+  ) {
+    if (!parentModule) {
+      this._badgeIconService.appendSvg();
+    }
+  }
+}
 
---
+++ src/badge/services/badge-icon.service.ts
@@ -0,0 +1,52 @@
+import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
+
+@Injectable()
+export class BadgeIconService {
+
+  private _svgPath = [
+    {
+      id: 'clip0',
+      path: 'M16.9996 0C16.3719 0.835661 16 1.87439 16 3C16 5.76142 18.2386 8 21 8C22.1256 8 23.1643 7.62806 24 7.00037V24H0V0H16.9996Z'
+    },
+    {
+      id: 'clip1',
+      path: 'M10.8321 0C10.2969 1.22492 10 2.57779 10 4C10 9.52285 14.4772 14 20 14C21.4222 14 22.7751 13.7031 24 13.1679V24H0V0H10.8321Z',
+    },
+    {
+      id: 'clip2',
+      path: 'M4.83209 0C4.2969 1.22492 4 2.57779 4 4C4 9.52285 8.47715 14 14 14H20C21.4222 14 22.7751 13.7031 24 13.1679V24H0V0H4.83209Z'
+    },
+    {
+      id: 'clip3',
+      path: 'M0 11.1414V24H24V13.1679C22.7751 13.7031 21.4222 14 20 14H7C4.27455 14 1.80375 12.9097 0 11.1414Z'
+    }
+  ];
+
+  private _r: Renderer2;
+
+  constructor(private _rFactory: RendererFactory2) {
+    this._r = _rFactory.createRenderer(null, null);
+  }
+
+  public appendSvg(): void {
+    const svg = this._r.createElement('svg', 'svg');
+    this._r.setAttribute(svg, 'width', '0');
+    this._r.setAttribute(svg, 'height', '0');
+
+    this._svgPath.forEach(i => {
+      const clip = this._r.createElement('clipPath', 'svg');
+      this._r.setAttribute(clip, 'id', i.id);
+
+      const path = this._r.createElement('path', 'svg');
+      // this._r.setAttribute(path, 'fill-rule', 'evenodd');
+      // this._r.setAttribute(path, 'clip-rule', 'evenodd');
+      this._r.setAttribute(path, 'd', i.path);
+      this._r.appendChild(clip, path);
+
+      this._r.appendChild(svg, clip);
+    });
+
+    this._r.appendChild(document.body, svg);
+  }
+
+}
