34f76aaf7d904e100d38d9c2ff78e7c0a24125ae
popenkov <popenkov@fuib.com>
2020-03-16 15:00:44 +0200

    overview: (checkbox, pin-input)

--- src/checkbox/checkbox.stories.ts
+++ src/checkbox/checkbox.stories.ts
@@ -4,46 +4,27 @@ import { withKnobs, boolean, text } from '@storybook/addon-knobs/angular';
 import { CheckboxComponent } from './components/checkbox/checkbox.component';
 import { checkboxWithValueAccessor } from './stories/checkbox-value-accessor';
 import { checkboxLoad } from './stories/checkobx-load';
+import { CheckboxOverviewComponent } from './examples/checkbox-page.component';
 
 const checkboxStories = storiesOf('Checkbox', module);
 checkboxStories.addDecorator(withKnobs);
 
-checkboxStories.add('regular checkbox', () => ({
+checkboxStories.add('Overview', () => ({
   moduleMetadata: {
     declarations: [
-      CheckboxComponent
+      CheckboxComponent,
+      CheckboxOverviewComponent
     ]
   },
   props: {
     onChange: action('Change fired!'),
     onIndeterminateChange: action('Indeterminate change fired!'),
-    label: text('List label name', 'Checkbox List'),
+    label: text('List label name', 'Checkbox'),
     hideLabel: boolean('Hide labels', false),
   },
   template: `
-    <fieldset class="fieldset">
-			<legend class="label">{{label}}</legend>
-			<checkbox
-				checked="true"
-				[hideLabel]="hideLabel"
-				(changeEventEmitter)="onChange($event)">
-				Checkbox
-			</checkbox>
-			<checkbox
-				indeterminate="true"
-				(changeEventEmitter)="onChange($event)"
-				[hideLabel]="hideLabel"
-				(indeterminateChange)="onIndeterminateChange($event)">
-				Indeterminate checkbox
-			</checkbox>
-			<checkbox
-				disabled="true"
-				(changeEventEmitter)="onChange($event)"
-				[hideLabel]="hideLabel"
-				(indeterminateChange)="onIndeterminateChange($event)">
-				Disabled checkbox
-			</checkbox>
-		</fieldset>
+      
+      <app-checkbox-overview [label]="label"></app-checkbox-overview>
   `
 }));
 
