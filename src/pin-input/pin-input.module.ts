1432b3f34f8ee39a03912d8f14ad894372dad55c
popenkov <popenkov@fuib.com>
2020-03-20 14:35:16 +0200

    fix preview pin-input component

--- src/pin-input/pin-input.module.ts
+++ src/pin-input/pin-input.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PinInputComponent } from './components/pin-input/pin-input.component';
 import { CommonModule } from '@angular/common';
+import { PinInputOverviewComponent } from './examples/pin-input-page.component';
 
 @NgModule({
     imports: [
         CommonModule
     ],
     declarations: [
-        PinInputComponent
+        PinInputComponent,
+        PinInputOverviewComponent
     ],
     exports: [
         PinInputComponent
