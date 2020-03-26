15ed2d3db9e5265183c77ceafc45b7c06b2902d1
Авдєєнко Дмитро Дмитрович <dmytro.avdeenko@fuib.com>
2020-03-26 11:41:18 +0200

    DSYS-29: Added preview page

--- src/switcher/switcher.stories.ts
+++ src/switcher/switcher.stories.ts
@@ -1,20 +1,18 @@
 import { storiesOf } from '@storybook/angular';
-import { withKnobs, boolean } from "@storybook/addon-knobs";
+import { withKnobs } from "@storybook/addon-knobs";
 import { SwitcherModule } from './switcher.module';
+import { SwitcherOverview } from './examples/switcher-overview/switcher-overview.component';
 
 
 const switcherStories = storiesOf('Switcher', module);
 switcherStories.addDecorator(withKnobs);
 
-switcherStories.add('switcher', () => ({
+switcherStories.add('Overview', () => ({
 	moduleMetadata: {
+	  declarations: [SwitcherOverview],
 		imports: [SwitcherModule]
 	},
-	props: {
-		value: boolean('on/off', false),
-		status: boolean('disabled?', false)
-	},
 	template: `
-		<mill-switcher [active]="value" [disabled]="status"></mill-switcher>
+		<switcher-overview ></switcher-overview>
     `
 }));
