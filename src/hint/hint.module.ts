003e615bf78a096ef1119f62b8f9537e5bfabba1
Igor Blazheevskiy <Igor.Blazgeevskyy@fuib.com>
2020-08-06 14:32:25 +0300

    fix: module deps

--- src/hint/hint.module.ts
+++ src/hint/hint.module.ts
@@ -3,15 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HintComponent } from './components/hint/hint.component';
 import { HintControlDirective } from './directives/hint-control.directive';
 import { IconsModule } from '../icons/icons.module';
-
-
+import { SelectModule } from '../select';
 
 @NgModule({
   declarations: [HintComponent, HintControlDirective],
   exports:  [HintComponent, HintControlDirective],
   imports: [
     CommonModule,
-    IconsModule
+    IconsModule,
+    SelectModule
   ]
 })
 export class MillHintModule { }
