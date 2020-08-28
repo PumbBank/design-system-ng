8d54dd08be375e9fc3cfe689f49380e392e5c503
popenkov <popenkov@fuib.com>
2020-08-28 18:15:18 +0300

    hot fix

--- src/input/examples/input-form/input-form.component.ts
+++ src/input/examples/input-form/input-form.component.ts
@@ -8,6 +8,14 @@ import { FormControl, ValidationErrors, Validators, FormGroup } from '@angular/f
 })
 
 export class InputFormComponent implements OnInit, AfterViewInit {
+  noticeItems: string[] = [
+    'Якщо обов\'язкове поле в фокусі, а потім фокус знімається без введення даних – повідомлення про обов\'язкове поле не виводиться.',
+    'Якщо обов\'язкове поле в фокусі, потім було зроблено введення даних, потім ці дані були стерті і фокус знятий – виводиться повідомлення про обов\'язкове поле.',
+    'Затвердження обов\'язкових полів відбувається при натисканні кнопки Submit.',
+    'При помилках в полях введення (в т.ч. про обов\'язкове поле) – екран проскролюється до самої верхньої помилки.',
+    'При відкритті форми – найперше верхнє поле введення, за замовчуванням виявляється у фокусі.'
+  ];
+
   formGr: FormGroup = new FormGroup({
     name: new FormControl('', Validators.required),
     walletNumber: new FormControl('', Validators.required),
