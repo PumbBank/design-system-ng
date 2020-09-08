5168087f227eb4335d8117f02746072ee3709fe2
avdeenkd <dmyto.avdeenko@fuib.com>
2020-09-08 17:37:05 +0300

    DSYS-99: update Table component with new functionality

--- src/data-table/public-api.ts
+++ src/data-table/public-api.ts
@@ -1,5 +1,1 @@
 export * from './data-table.module';
-export * from './data-table-source';
-export * from './mill-paginator/mill-paginator.component';
-export * from './mill-sort/mill-sort';
-export * from './mill-sort/mill-sort-header/mill-sort-header.component';
