e6c254ca4e39186ca9653b4933f8bd6a567c089b
Valdemar Kostenko <volodymyr.kostenko@fuib.com>
2021-02-07 19:09:39 +0200

    DSYS-196: switch Spinner component into SVG realisation

--- src/spinner/components/spinner/spinner.component.ts
+++ src/spinner/components/spinner/spinner.component.ts
@@ -1,17 +1,15 @@
-import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
 
 @Component({
   selector: 'mill-spinner',
   templateUrl: './spinner.component.html',
   styleUrls: ['./spinner.component.scss']
 })
-export class SpinnerComponent implements OnInit {
+export class SpinnerComponent {
   @HostBinding('class') public hostClass: string = 'mill-spinner';
   @Input() label: string;
+  @Input() description: string;
+  @Input() size: 'small' | 'medium' | 'big' = 'medium';
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
 }
