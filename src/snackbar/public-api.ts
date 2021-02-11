b9bd172aa870210839557b1561d446a1acaf6863
Igor Blazheevskiy <Igor.Blazgeevskyy@fuib.com>
2021-02-11 16:45:10 +0200

    fix: fix imports

--- src/snackbar/public-api.ts
+++ src/snackbar/public-api.ts
@@ -1,3 +1,4 @@
 export * from './components/snackbar/snackbar.component';
 export * from './snackbar.module';
 export * from './models/snackbar-config.model';
+export * from './services/snackbar.service';
