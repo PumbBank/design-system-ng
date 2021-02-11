dae4eb8aaeecad1f27b01c1f5894096840f45e8c
Valdemar Kostenko <volodymyr.kostenko@fuib.com>
2021-02-11 14:03:20 +0200

    DSYS-206: fix for public-api in Data-Table, v0.10.51

--- src/data-table/public-api.ts
+++ src/data-table/public-api.ts
@@ -1,5 +1,5 @@
 export * from './data-table.module';
 export * from './data-table-source';
-export {MillPaginatorComponent as MillPaginator} from './mill-paginator/mill-paginator.component';
-export {MillSortToken, MillSortDirective as MillSort} from './mill-sort/mill-sort.directive';
 export * from './mill-sort/mill-sort-header/mill-sort-header.component';
+export { PaginatorInterface, MillPaginatorComponent as MillPaginator } from './mill-paginator/mill-paginator.component';
+export { SortInterface, MillSortToken, MillSortDirective as MillSort } from './mill-sort/mill-sort.directive';
